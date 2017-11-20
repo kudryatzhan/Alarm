3d9a55086080eef2c7bbda89b211ff58f13b93a3
Kudryatzhan Arziyev <karziyev@gmail.com>
2017-11-20 14:46:19 -0700

    Views set up

---
+++ alarm/View Controllers/AlarmDetailTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AlarmDetailTableViewController.swift
+//  alarm
+//
+//  Created by Kudryatzhan Arziyev on 11/20/17.
+//  Copyright © 2017 Kudryatzhan Arziyev. All rights reserved.
+//
+
+import UIKit
+
+class AlarmDetailTableViewController: UITableViewController {
+
+    // Outlets
+    @IBOutlet weak var alarmDatePicker: UIDatePicker!
+    @IBOutlet weak var alarmTitleTextField: UITextField!
+    @IBOutlet weak var enableButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func enableButtonTapped(_ sender: UIButton) {
+    }
+    
+}
---
+++ alarm/Views/SwitchTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  SwitchTableViewCell.swift
+//  alarm
+//
+//  Created by Kudryatzhan Arziyev on 11/20/17.
+//  Copyright © 2017 Kudryatzhan Arziyev. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var alarmSwitch: UISwitch!
+    
+    @IBAction func switchValueChanged(_ sender: UISwitch) {
+    }
+}
