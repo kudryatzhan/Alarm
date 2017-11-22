c5a169f3cbf6f81f4f6017964968aacd465d7241
Kudryatzhan Arziyev <karziyev@gmail.com>
2017-11-21 21:47:28 -0700

    Registered app for UserNotifications

--- alarm/AppDelegate.swift
+++ alarm/AppDelegate.swift
@@ -7,15 +7,23 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let userNotificationCenter = UNUserNotificationCenter.current()
+        userNotificationCenter.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+            if !granted {
+                print("Notification has been denied.")
+            }
+        }
+        
         return true
     }
 
